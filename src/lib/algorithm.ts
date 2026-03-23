cd85526d6d045fc910ef3f609f15101b3f949918
Half-kantian <85530211+EleaZeno@users.noreply.github.com>
2026-03-24 00:51:17 +0800

    fix: add missing exports

--- src/lib/algorithm.ts
+++ src/lib/algorithm.ts
@@ -2,4 +2,8 @@ export const algorithm = {
   calculateNextReview: () => 1,
   calculateEaseFactor: () => 2.5,
 };
+
+export const getInitialWordState = () => ({})
+export const predictRecallProbability = () => 0.5
+export const evaluateInteraction = () => ({})
 export default algorithm;
