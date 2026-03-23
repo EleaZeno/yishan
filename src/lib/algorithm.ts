3092a80320ed0824e57131b1a0d66e96499c5f8a
Half-kantian <85530211+EleaZeno@users.noreply.github.com>
2026-03-24 04:07:16 +0800

    restore: original src\lib\algorithm.ts

--- src/lib/algorithm.ts
+++ src/lib/algorithm.ts
@@ -2,8 +2,4 @@ export const algorithm = {
   calculateNextReview: () => 1,
   calculateEaseFactor: () => 2.5,
 };
-
-export const getInitialWordState = () => ({})
-export const predictRecallProbability = () => 0.5
-export const evaluateInteraction = () => ({})
 export default algorithm;
