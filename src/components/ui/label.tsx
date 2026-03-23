c748a33830044e82131b6d6436c94232575bbf71
Half-kantian <85530211+EleaZeno@users.noreply.github.com>
2026-03-24 04:06:35 +0800

    restore: original src\components\ui\label.tsx

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+"use client"
 
-export function label(props: any) {
-  return <div {...props}>label</div>;
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+function Label({ className, ...props }: React.ComponentProps<"label">) {
+  return (
+    <label
+      data-slot="label"
+      className={cn(
+        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+        className
+      )}
+      {...props}
+    />
+  )
 }
+
+export { Label }
