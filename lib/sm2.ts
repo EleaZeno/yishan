7fd909c54eba96e10c80e5da8052291af7965be5
Half-kantian <85530211+EleaZeno@users.noreply.github.com>
2026-03-24 00:24:54 +0800

    chore: remove old file - lib/sm2.ts

--- lib/sm2.ts
+++
@@ -1,9 +0,0 @@
-// Deprecated. Replaced by lib/algorithm.ts
-// Keeping this file to export getInitialWordState compatible with new format to prevent breakages in AddWordModal
-import { getInitialWordState as getAlgoInitialState } from './algorithm';
-
-export const calculateReview = () => {
-    throw new Error("SM-2 is deprecated. Use lib/algorithm.ts");
-}
-
-export const getInitialWordState = getAlgoInitialState;
