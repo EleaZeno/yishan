80272ff10bf131cdfb7a8a56c07f70b41349f0d9
Half-kantian <85530211+EleaZeno@users.noreply.github.com>
2026-03-24 03:53:28 +0800

    fix: add missing dropdown menu exports

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -11,3 +11,15 @@ export function DropdownMenuTrigger(props: any) {
 export function DropdownMenuContent(props: any) {
   return <div {...props}>Content</div>;
 }
+
+export function DropdownMenuItem(props: any) {
+  return <div {...props}>Item</div>;
+}
+
+export function DropdownMenuLabel(props: any) {
+  return <div {...props}>Label</div>;
+}
+
+export function DropdownMenuSeparator(props: any) {
+  return <hr {...props} />;
+}
