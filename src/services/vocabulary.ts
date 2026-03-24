f0ab3cba47f416c779e967c15187d1cbd3474cd8
Half-kantian <85530211+EleaZeno@users.noreply.github.com>
2026-03-24 21:20:54 +0800

    restore: src/services/vocabulary.ts from original commit

--- src/services/vocabulary.ts
+++ src/services/vocabulary.ts
@@ -1,22 +1,21 @@
-export interface VocabularyBook {
-  id: string;
-  name: string;
-  wordCount: number;
+export const vocabulary = []
+
+export const getAllWords = async () => vocabulary
+export const getDueWords = async () => []
+export const getAllWordsForStats = async () => []
+export const getLibraryWords = async () => []
+export const importWords = async (words: any[]) => {}
+export const addWord = async (word: any) => {}
+export const deleteWord = async (id: string) => {}
+export const updateWord = async (id: string, data: any) => {}
+
+export default {
+  getAllWords,
+  getDueWords,
+  getAllWordsForStats,
+  getLibraryWords,
+  importWords,
+  addWord,
+  deleteWord,
+  updateWord,
 }
-
-const BOOKS: VocabularyBook[] = [
-  { id: '1', name: 'CET-4', wordCount: 4000 },
-  { id: '2', name: 'CET-6', wordCount: 2000 },
-  { id: '3', name: 'IELTS', wordCount: 5000 },
-  { id: '4', name: 'TOEFL', wordCount: 8000 },
-];
-
-export async function getBooksWithFallback(): Promise<VocabularyBook[]> {
-  return BOOKS;
-}
-
-export async function getBookWordsWithFallback(bookId: string): Promise<any[]> {
-  return [];
-}
-
-export default { getBooksWithFallback, getBookWordsWithFallback };
